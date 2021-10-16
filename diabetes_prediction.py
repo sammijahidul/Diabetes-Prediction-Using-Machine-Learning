6446d36685c01722307c5f5e7f79c00cf3f14d81
sammijahidul <jahidul.islam03@northsouth.edu>
2021-10-16 11:49:54 +0600

    just imported neccessary libraries

--- diabetes_prediction.py
+++ diabetes_prediction.py
@@ -1,1 +1,7 @@
-
+# Importing neccessary libraries 
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
