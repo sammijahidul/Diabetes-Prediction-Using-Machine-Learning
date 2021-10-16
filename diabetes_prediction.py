3fab949732a56c481e6b67b9df01fad9971774bf
sammijahidul <jahidul.islam03@northsouth.edu>
2021-10-16 12:14:52 +0600

    importing dataset into dataframe & evaluate the output and input column in dataset

--- diabetes_prediction.py
+++ diabetes_prediction.py
@@ -5,3 +5,16 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
+
+## Data pre-processing part starts from here
+
+# Importing dataset into pandas dataframe
+data = pd.read_csv("diabetes.csv")
+data.head()
+data.describe()
+data.shape
+data['Outcome'].value_counts() # will show the output measurements 
+data.groupby('Outcome').mean() # will show the mean value for each column [O & 1]
+
+
+
